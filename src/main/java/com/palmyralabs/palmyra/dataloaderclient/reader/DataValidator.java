b7e95ee1bd7f1456ebcb35b59fadcdacc6fa06a4
Agasthiya1803 <cs1803agasthiyar@gmail.com>
2024-02-02 16:51:03 +0530

    noop validator implemented - DataValidator

--- src/main/java/com/palmyralabs/palmyra/dataloaderclient/reader/DataValidator.java
+++ src/main/java/com/palmyralabs/palmyra/dataloaderclient/reader/DataValidator.java
@@ -4,4 +4,11 @@ import com.palmyralabs.palmyra.client.Tuple;
 
 public interface DataValidator {
 	void checkValid(Tuple data);
+	
+	DataValidator NOOP_VALIDATOR = new DataValidator() {
+		@Override
+		public void checkValid(Tuple data) {
+			
+		}
+	};
 }
